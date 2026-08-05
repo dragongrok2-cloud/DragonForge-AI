eab53e3c88f5d6b22645bdabb2ea3c871ea814db
Grok Dragon <dragongrok2@gmail.com>
2026-08-05 10:14:48 +0300

    Export persistence helpers and bump version to 0.1.2

--- dragonforge/__init__.py
+++ dragonforge/__init__.py
@@ -3,6 +3,16 @@
 from .core.character import Character, DragonCharacter
 from .core.memory import MemoryForge
 from .core.soul import Soul
+from .core.persistence import save_character, load_character, character_to_dict, character_from_dict
 
-__version__ = "0.1.1"
-__all__ = ["Character", "DragonCharacter", "MemoryForge", "Soul"]
+__version__ = "0.1.2"
+__all__ = [
+    "Character",
+    "DragonCharacter",
+    "MemoryForge",
+    "Soul",
+    "save_character",
+    "load_character",
+    "character_to_dict",
+    "character_from_dict",
+]
